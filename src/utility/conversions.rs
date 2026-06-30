7af9ff3805be6913874bb6e18f86ee38e9380594
Orion4C <65980299+Orion4C@users.noreply.github.com>
2026-06-30 02:26:18 -0400

    🔥removed unused converstion functions

--- src/utility/conversions.rs
+++ src/utility/conversions.rs
@@ -1,16 +1,8 @@
 pub enum BytesConversion {}
 
 impl BytesConversion {
-    pub fn from_gb(gb: f32) -> f32 {
-        gb * 1000000000.0
-    }
-
     pub fn from_mb(mb: f32) -> f32 {
         mb * 1000000.0
-    }
-
-    pub fn to_gb(bytes: f32) -> f32 {
-        bytes / 1000000000.0
     }
 
     pub fn to_mb(bytes: f32) -> f32 {
